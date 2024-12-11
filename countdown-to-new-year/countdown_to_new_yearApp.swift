303c547fc597f20095757e7b162c0460d4398ee9
notosgnikusnsi <notosgnikusnsi@gmail.com>
2024-12-11 13:53:16 +0900

    Initial Commit

---
+++ countdown-to-new-year/countdown_to_new_yearApp.swift
@@ -0,0 +1,17 @@
+//
+//  countdown_to_new_yearApp.swift
+//  countdown-to-new-year
+//
+//  Created by Kozuno Tomoha on 2024/12/11.
+//
+
+import SwiftUI
+
+@main
+struct countdown_to_new_yearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
